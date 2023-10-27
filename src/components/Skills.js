cb85f65f5d191a6f117293c157bc67ac86640220
zahraa985 <zaa19650@gmail.com>
2023-10-27 03:20:44 -0700

    fixed skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,16 +1,12 @@
+import React from "react";
 import meter1 from "../assets/img/meter1.svg";
 import meter2 from "../assets/img/meter2.svg";
-import meter3 from "../assets/img/meter3.svg";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import arrow1 from "../assets/img/arrow1.svg";
-import arrow2 from "../assets/img/arrow2.svg";
-import colorSharp from "../assets/img/color-sharp.png";
 
 export const Skills = () => {
   const responsive = {
     superLargeDesktop: {
-      // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
       items: 5,
     },
@@ -35,27 +31,56 @@ export const Skills = () => {
           <div className="col-12">
             <div className="skill-bx wow zoomIn">
               <h2>Skills</h2>
-              <p>
-                <br></br>
-              </p>
-              <Carousel
-                responsive={responsive}
-                infinite={true}
-                className="owl-carousel owl-theme skill-slider"
-              >
-                <div className="item">
-                  <img src={meter1} alt="Image" />
-                  <h5>Web Development</h5>
-                </div>
-                <div className="item">
-                  <img src={meter2} alt="Image" />
-                  <h5>Data analysis</h5>
-                </div>
-                <div className="item">
-                  <img src={meter2} alt="Image" />
-                  <h5>Database management</h5>
-                </div>
-              </Carousel>
+              <div className="skill-list">
+                <Carousel
+                  responsive={responsive}
+                  infinite={true}
+                  className="owl-carousel owl-theme skill-slider"
+                >
+                  <div className="item">
+                    <img src={meter1} alt="Image" />
+                    <h5>Data Science</h5>
+                    <p>
+                      <strong>Languages:</strong> Python, R, SQL
+                    </p>
+                    <p>
+                      <strong>Data Manipulation:</strong> Data cleaning, data
+                      wrangling
+                    </p>
+                    <p>
+                      <strong>Data Analysis:</strong> Data visualization,
+                      statistical modeling
+                    </p>
+                    <p>
+                      <strong>Machine Learning:</strong> Algorithms, model
+                      building and evaluation
+                    </p>
+                    <p>
+                      <strong>Mathematical Modeling:</strong> Optimization,
+                      Simulation
+                    </p>
+                  </div>
+                  <div className="item">
+                    <img src={meter2} alt="Image" />
+                    <h5>Web Development</h5>
+                    <p>
+                      <strong>Languages:</strong> JavaScript (React), HTML, CSS
+                    </p>
+                    <p>
+                      <strong>Frameworks:</strong> React
+                    </p>
+                    <p>
+                      <strong>Server-side programming</strong>
+                    </p>
+                    <p>
+                      <strong>RESTful APIs</strong>
+                    </p>
+                    <p>
+                      <strong>Version Control:</strong> Git
+                    </p>
+                  </div>
+                </Carousel>
+              </div>
             </div>
           </div>
         </div>
